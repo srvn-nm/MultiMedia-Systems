ddfc31bedc55d68b03c5ea7ef9a1053d0fce4087
sarvin nami <srvn0nm@gmail.com>
2024-06-06 22:55:38 +0330

    delete the old files

--- HW3/inverse_quantize_dct_coefficients.py
+++
@@ -1,9 +0,0 @@
-def inverse_quantize_dct_coefficients(quantized_dct_coefficients, quant_matrix):
-    # Ensure the quantized DCT coefficients and quantization matrix have the correct shape
-    if quantized_dct_coefficients.shape[2:] != quant_matrix.shape:
-        raise ValueError("The shape of the quantization matrix must match the shape of the DCT blocks.")
-
-    # Perform inverse quantization by element-wise multiplication
-    inverse_quantized_coefficients = quantized_dct_coefficients * quant_matrix
-
-    return inverse_quantized_coefficients
